886a4ca20fe303ec5cfdbd16f9cecab5e2eb4a32
mike mendes <4mikemendes@gmail.com>
2021-11-22 15:12:21 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar.js"
+import Cards from "./components/Cards.js"
+
+  
+// create a data.js with id, location, google maps, link, start date
+//end date, description, image URL
+// use .map() and props
+//style & polish
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <Cards />
     </div>
   );
 }
